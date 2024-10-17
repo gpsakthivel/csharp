388faf4b164f4c3bf9c94f253fa4ae0d04289cae
gpsakthivel <psakthi24vel@gmail.com>
2024-10-17 21:05:42 +0530

    Added console application in csharp

---
+++ Giraffe1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Giraffe1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello world code
+            Console.WriteLine("Hello, World!");
+
+            // Draw a shape
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            // Variables
+            Console.WriteLine("There once was a man George");
+            Console.WriteLine("He is 70 years old");
+            Console.WriteLine("He really liked the name George");
+            Console.WriteLine("He didn't really like being 70");
+
+            string characterName = "John";
+            int characterAge;
+            characterAge = 50;
+            Console.WriteLine("There once was a man " + characterName);
+            Console.WriteLine("He is " + characterAge + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("He didn't really like being " + characterAge);
+        }
+    }
+}
